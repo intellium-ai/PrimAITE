a1a169e147a5baae433a7b9ad3f601265dcc6020
Jack Smith <jack.smith@intellium.ai>
2024-06-11 16:00:39 +0000

    cli

--- app/run.py
+++ app/run.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from primaite.main import run
 
 config_path = Path("../src/primaite/config/_package_data/")
-training_config_path = config_path / "training" / "training_config_main.yaml"
+training_config_path = config_path / "training" / "sb3.yaml"
 lay_down_config_path = config_path / "lay_down" / "lay_down_config_5_data_manipulation.yaml"
 
 run(training_config_path=training_config_path, lay_down_config_path=lay_down_config_path)
