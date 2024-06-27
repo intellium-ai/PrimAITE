54da7f33a24e3b063e95ccf703e8de3a39735adc
Jack Smith <jack.smith@intellium.ai>
2024-06-27 10:57:41 +0000

    eek

--- src/primaite/graph/generate.py
+++ src/primaite/graph/generate.py
@@ -1,7 +1,7 @@
 import random
 from pathlib import Path
 from typing import List, Dict, Any
-
+import numpy as np
 import yaml
 
 from primaite.common.enums import (
@@ -20,58 +20,45 @@ class NetworkGenerator:
     ports: List[int]
     nodes: List[Node]
     links: List[Link]
+    node_idx: int
 
-    def __init__(self, num_nodes: int, num_services: int, num_links: int) -> None:
-        self.num_nodes = num_nodes
-        self.num_services = num_services
-        self.num_links = num_links
+    def __init__(self) -> None:
+        self.node_idx = 1
 
-    def add_ports_and_services(self, low=1, high=99) -> None:
-        if self.num_services > len(list(Service)):
-            raise ValueError
+    def add_port_and_service(self, low=1, high=99) -> None:
 
-        self.services = random.sample(list(Service), self.num_services)
-        self.ports = random.sample(range(low, high), self.num_services)
+        self.services.append(list(Service)[np.random.randint(low=0, high=len(list(Service)))])
+        self.ports.append(np.random.randint(low=low, high=high))
 
-    def add_nodes(self) -> None:
+    def add_node(self) -> None:
         self.nodes = []
 
-        if not hasattr(self, "services"):
-            self.add_ports_and_services()
-
         node_types = list(NodeType)
+        node_type = node_types[np.random.randint(low=0, high=len(node_types))]
 
-        for i in range(1, self.num_nodes + 1):
+        sampler = random.sample
 
-            node_type_idx = (i - 1) % len(node_types)
-            node_type = node_types[node_type_idx]
+        node_service = NodeService(
+            name=sampler(self.services, 1),
+            port=self.ports[0],
+            state=SoftwareState.GOOD,
+        )
 
-            node_service = NodeService(
-                name=self.services[0],
-                port=self.ports[0],
-                state=SoftwareState.GOOD,
-            )
+        node = Node(
+            node_id=self.node_idx,
+            name="PC",
+            node_type=node_type,
+            priority=Priority.P1,
+            hardware_state=HardwareState.ON,
+            ip_address=f"192.168.0.{self.node_idx}",
+            software_state=SoftwareState.GOOD,
+            file_system_state=FileSystemState.GOOD,
+            services=[node_service],
+        )
 
-            node = Node(
-                node_id=i,
-                name="PC",
-                node_type=node_type,
-                priority=Priority.P1,
-                hardware_state=HardwareState.ON,
-                ip_address=f"192.168.0.{i}",
-                software_state=SoftwareState.GOOD,
-                file_system_state=FileSystemState.GOOD,
-                services=[node_service],
-            )
+        self.nodes.append(node)
 
-            self.nodes.append(node)
-
-    def add_links(self):
-        self.links = []
-
-        for i in range(self.num_nodes + 1, self.num_nodes + self.num_links + 1):
-            link = Link(id=i, name=f"LINK_{i - self.num_nodes}", source=1, destination=2)
-            self.links.append(link)
+        self.node_idx += 1
 
     def _ports_to_dict(self) -> Dict[str, Any]:
         port_config = [{"port": f"{port}"} for port in self.ports]
@@ -98,18 +85,13 @@ class NetworkGenerator:
 
     def save(self, path: str | Path = "test.yaml") -> None:
 
-        if not hasattr(self, "nodes"):
-            self.add_ports_and_services()
-            self.add_nodes()
-            self.add_links()
-
         with open(path, "w+") as f:
             yaml.safe_dump(self.dict(), f)
 
 
 def main():
 
-    network = NetworkGenerator(num_nodes=3, num_services=2, num_links=1)
+    network = NetworkGenerator()
     network.save("src/primaite/config/_package_data/lay_down/test.yaml")
 
 
